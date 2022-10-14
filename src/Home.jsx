c861e415a6c7a1947e4b15e2efbb3610339e6539
Mandeep Kaur <mandeep.kaur@bayatree.com>
2022-10-14 22:02:02 +0530

    favicon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,14 @@
+import Favicon from "react-favicon";
+import FavIcon from "./assets/favicon.png";
 import Header from "./viewport/header";
 import Footer from "./viewport/footer";
 import Main from "./viewport/main";
 
 function App() {
   return (
+    
     <div className="wrapper">
+       <Favicon url={FavIcon}></Favicon>
       <Header />
 
       <Main />
